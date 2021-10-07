3ff0e8d8eca5309c7f7ca56aeafaacde6535453d
VladyslavStashkevych <91185287+VladyslavStashkevych@users.noreply.github.com>
2021-10-07 13:24:35 +0300

    Add files via upload

---
+++ lab4.1/lab4.1/lab4.1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int i, k, N; 
+	double sum = 0;
+
+	cout << "Enter k: "; cin >> k;
+	cout << "Enter N: "; cin >> N;
+
+	i = k;
+
+	while (i <= N) {
+		sum += sin(i) * cos (i) / (1 + pow(sin(i), 2));
+		i++;
+	}
+
+	cout << "Result 1: " << sum << endl;
+	sum = 0;
+	i = k;
+
+	do {
+		sum += sin(i) * cos(i) / (1 + pow(sin(i), 2));
+		i++;
+	} while (i <= N);
+
+	cout << "Result 2: " << sum << endl;
+	sum = 0;
+
+	for (i = k; i <= N; i++) {
+		sum += sin(i) * cos(i) / (1 + pow(sin(i), 2));
+	}
+
+	cout << "Result 3: " << sum << endl;
+	sum = 0;
+
+	for (i = N; i >= k; i--) {
+		sum += sin(i) * cos(i) / (1 + pow(sin(i), 2));
+	}
+
+	cout << "Result 4: " << sum << endl;
+
+	return 0;
+}
